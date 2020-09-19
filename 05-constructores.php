9470ca24f857943c1a3b0b3c5ab83534f56799d5
Norman Arredondo Martinez <arredondo_norman25@outlook.es>
2020-09-19 00:50:11 -0500

    Trabajando con un contructor

--- 05-constructores.php
+++ 05-constructores.php
@@ -1,10 +1,19 @@
 <?php include 'includes/header.php';
 
+//Contructor: Es una función que se ejecuta automaticamente en el momento en el que creas un objeto en una clase
+//En php versiones anteriores o en Java, (se utiliza como constructor el nombre de la clase) pero PHP tiene un nombre reservado __construct()
 class MenuRestaurant {
     public $nombre;
     public $precio;
+    public $categoria;
 
-    public function getNombre() 
+    public function __construct($nombre, $precio, $categoria){
+      $this->nombre = $nombre;
+      $this->precio = $precio;
+      $this->$categoria = $categoria;
+    }
+
+    public function getNombre()
     {
         return $this->nombre;
     }
@@ -14,3 +23,7 @@ class MenuRestaurant {
         return $this->precio;
     }
 }
+
+//Instanciar
+$bebida = new MenuRestaurant("Jugo de Uva",30,'Bebidas');
+echo $bebida->getNombre();
