5828fd39c9891096486f44948151d058e944fcf6
Norman Arredondo Martinez <arredondo_norman25@outlook.es>
2020-09-23 01:32:02 -0500

    Método static, no requiere que se instancie la clase pero requiere el nombre de la clase seguido de :: y después el método

--- 10-static.php
+++ 10-static.php
@@ -1,1 +1,24 @@
 <?php include 'includes/header.php';
+
+/*Métodos estáticos
+ *No se requiere que se instancie la clase
+ * Pero requiere el nombre de la clase seguido de :: y después el método
+ * */
+
+class DB {
+    public static function conectarDB(){
+        echo "Conectando a la base de datos";
+    }
+}
+
+class Email {
+    public static function enviarEmail() {
+        echo "Enviando email...";
+    }
+}
+
+DB::conectarDB();
+
+echo "<br>";
+
+Email::enviarEmail();
